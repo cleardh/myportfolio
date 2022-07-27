53ee1383a4f021a90270cd3b69292e81ffbc17e0
Dongha Kang <dongha@irisbooth.com>
2022-07-27 16:58:44 -0400

    Added unsafe-perm true before npm install

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -4,4 +4,5 @@ export NVM_DIR="/home/ubuntu/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 cd /home/ubuntu/portfolio
+npm config set unsafe-perm=true
 npm install
